e8198bf341eda327872a9f25c058af6e7a9f9297
John Tokhi <56596400+tokhij@users.noreply.github.com>
2019-12-26 12:50:37 -0500

    started part 5

--- src/main/java/com/example/demo/Instructor.java
+++ src/main/java/com/example/demo/Instructor.java
@@ -1,4 +1,14 @@
 package com.example.demo;
 
 public class Instructor extends Person implements Teacher {
+
+    public void teach(Learner learner, double numberOfHours){
+
+    }
+
+    public void lecture(Iterable<? extends Learner> learners, double numberOfHours){
+        double numberOfHoursPerLearner = numberOfHours / learners;
+    }
+
+
 }
