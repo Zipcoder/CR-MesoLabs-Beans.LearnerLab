a4364f24905eceed8c4f4553bbccc1056243210c
Jbanksalpha <78812718+Jbanksalpha@users.noreply.github.com>
2021-04-23 13:45:03 -0400

    Created Test classes for config classes

--- demo/src/test/java/com/example/demo/InstructorConfigTest.java
+++ demo/src/test/java/com/example/demo/InstructorConfigTest.java
@@ -1,10 +1,47 @@
 package com.example.demo;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class InstructorConfigTest {
+    @Autowired
+    @Qualifier("instructors")
+    Instructors instructors;
+
+    @Autowired
+    @Qualifier("tcUsaInstructors")
+    Instructors tcUsaInstructors;
+
+    @Autowired
+    @Qualifier("tcUkInstructors")
+    Instructors tcUkInstructors;
+
+    @Test
+    public void instructors() {
+
+        Assert.assertEquals("The Sleuth", instructors.findBYId(84).getName());
+    }
+
+    @Test
+    public void tcUsaInstructors () {
+        Assert.assertEquals("John Bob", tcUsaInstructors.findBYId(10).getName());
+    }
+
+    @Test
+    public void tcUkInstructors () {
+        Assert.assertEquals("Harry Sussex", tcUkInstructors.findBYId(88).getName());
+    }
+
+
+
+
+
+
 }
--- demo/src/test/java/com/example/demo/TestStudentConfig.java
+++ demo/src/test/java/com/example/demo/TestStudentConfig.java
@@ -1,13 +1,39 @@
 package com.example.demo;
 
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestStudentConfig {
+    @Autowired
+    @Qualifier("students")
+    Students currentStudents;
+
+
+    @Autowired
+    @Qualifier("previousStudents")
+    Students previousStudents;
+
+    @Test
+    public void students() {
+
+        Assert.assertEquals("Justin Banks", currentStudents.findBYId(1).getName());
+    }
+    @Test
+    public void priorStudents() {
+
+        Assert.assertEquals("This one", previousStudents.findBYId(99).getName());
+    }
+
+
+
 
 
 }
