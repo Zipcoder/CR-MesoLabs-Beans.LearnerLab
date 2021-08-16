be13627fa517ea7f7221947a51854e311bb251bc
jgiroso <jrgiroso@gmail.com>
2021-08-16 12:10:49 -0400

    student class created

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+public class Student extends Person implements Learner{
+
+    double totalStudyTime;
+
+    public Student(Long id) {
+        super(id);
+    }
+
+    public Student(Long id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public void learn(double numberOfHours) {
+        this.totalStudyTime += numberOfHours;
+    }
+
+    public double getTotalStudyTime() {return totalStudyTime;}
+}
