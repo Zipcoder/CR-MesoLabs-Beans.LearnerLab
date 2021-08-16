c586280be59c5711e226fea0f2ca0fbd568f005c
Zach <zach@zipcodes-MacBook-Pro-6.local>
2021-08-16 12:38:12 -0400

    completed section 5, Instructor class

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public class Student extends Person implements Learner{
+
+    private double totalStudyTime = 0;
+
+    public Student(Long id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public void learn(double numberOfHours) {
+        totalStudyTime += numberOfHours;
+    }
+}
