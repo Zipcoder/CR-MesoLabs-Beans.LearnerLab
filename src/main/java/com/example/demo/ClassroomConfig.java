8ea93db4070acfe9fca33653a734bddb6b5f64da
Zach <zach@zipcodes-MacBook-Pro-6.local>
2021-08-16 18:11:15 -0400

    Completed ClassroomConfig tests

--- src/main/java/com/example/demo/ClassroomConfig.java
+++ src/main/java/com/example/demo/ClassroomConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
@@ -12,13 +13,13 @@ public class ClassroomConfig {
 
     @Bean(name = "currentCohort")
     @DependsOn({"instructors", "students"})
-    public Classroom currentCohort(){
-        return currentCohort;
+    public Classroom currentCohort(Instructors instructor, Students students){
+        return new Classroom(instructor, students);
     }
 
     @Bean(name = "previousCohort")
-    @DependsOn({"instructors", "students"})
-    public Classroom previousCohort(){
-        return previousCohort;
+    @DependsOn({"instructors", "previousStudents"})
+    public Classroom previousCohort(Instructors instructors,@Qualifier("previousStudents") Students students){
+        return new Classroom(instructors, students);
     }
 }
